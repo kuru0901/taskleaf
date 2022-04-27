64df3ab7c07cb8789abb51b9434fc52fb2e33850
kuru0901 <nyo0901wa@yahoo.co.jp>
2022-04-27 21:07:50 +0900

    to chapter4-3-4

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,10 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_04_22_235954) do
+ActiveRecord::Schema.define(version: 2022_04_27_092926) do
 
   create_table "tasks", force: :cascade do |t|
-    t.string "name"
+    t.string "name", limit: 30, null: false
     t.text "description"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
